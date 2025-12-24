60b3b77ea151bd4e70fa862d6aae2315786b0a7e
Artiom Vasilenko <artiomvasilenko@gmail.com>
2025-12-25 00:01:37 +0400

    add page "Not Found" and lazyLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,56 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Layout from "./components/Layout";
-import Home from "./modules/Home";
-import Calculator from "./modules/Calculator";
-import Timer from "./modules/Timer";
-import RandomNumberGenerator from "./modules/RandomNumberGenerator";
-import PasswordGenerator from "./modules/PasswordGenerator";
-import CreditCalculator from "./modules/CreditCalcilator";
-import InvestmentCalculator from "./modules/InvestmentCalculator";
-import TraderCalculator from "./modules/TraderCalculator";
-import Calendar from "./modules/Calendar";
-import CarSaleContractGenerator from "./modules/CarSaleContractGenerator";
+import SpinnerLoading from "./components/SpinnerLoading";
+
+const Home = lazy(() => import("./modules/Home"));
+const Calculator = lazy(() => import("./modules/Calculator"));
+const Timer = lazy(() => import("./modules/Timer"));
+const RandomNumberGenerator = lazy(() =>
+  import("./modules/RandomNumberGenerator")
+);
+const PasswordGenerator = lazy(() => import("./modules/PasswordGenerator"));
+const CreditCalculator = lazy(() => import("./modules/CreditCalculator"));
+const InvestmentCalculator = lazy(() =>
+  import("./modules/InvestmentCalculator")
+);
+const TraderCalculator = lazy(() => import("./modules/TraderCalculator"));
+const Calendar = lazy(() => import("./modules/Calendar"));
+const CarSaleContractGenerator = lazy(() =>
+  import("./modules/CarSaleContractGenerator")
+);
+const NotFound = lazy(() => import("./components/NotFound"));
+
+const lazyLoad = (component) => (
+  <Suspense fallback={<SpinnerLoading />}>{component}</Suspense>
+);
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Home /> },
-      { path: "calculator", element: <Calculator /> },
-      { path: "timer", element: <Timer /> },
-      { path: "number_random_generator", element: <RandomNumberGenerator /> },
-      { path: "password_generator", element: <PasswordGenerator /> },
-      { path: "credit_calculator", element: <CreditCalculator /> },
-      { path: "investment_calculator", element: <InvestmentCalculator /> },
-      { path: "trader_calculator", element: <TraderCalculator /> },
-      { path: "calendar", element: <Calendar /> },
-      { path: "calendar/:year", element: <Calendar /> },
-      { path: "dkp", element: <CarSaleContractGenerator /> },
+      {
+        index: true,
+        element: lazyLoad(<Home />),
+      },
+      { path: "calculator", element: lazyLoad(<Calculator />) },
+      { path: "timer", element: lazyLoad(<Timer />) },
+      {
+        path: "number_random_generator",
+        element: lazyLoad(<RandomNumberGenerator />),
+      },
+      { path: "password_generator", element: lazyLoad(<PasswordGenerator />) },
+      { path: "credit_calculator", element: lazyLoad(<CreditCalculator />) },
+      {
+        path: "investment_calculator",
+        element: lazyLoad(<InvestmentCalculator />),
+      },
+      { path: "trader_calculator", element: lazyLoad(<TraderCalculator />) },
+      { path: "calendar", element: lazyLoad(<Calendar />) },
+      { path: "calendar/:year", element: lazyLoad(<Calendar />) },
+      { path: "dkp", element: lazyLoad(<CarSaleContractGenerator />) },
+      { path: "*", element: lazyLoad(<NotFound />) },
     ],
   },
 ]);
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h1 className="text-8xl">Такой страницы не существует!</h1>
+      <p className="text-gray-400">Что ты здесь хотел увидеть?</p>
+    </div>
+  );
+}
+
+export default NotFound;
