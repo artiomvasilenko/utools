c5892937fbe9c0eb24a1a91b87946f27f8d46c72
Artiom Vasilenko <artiomvasilenko@yandex.ru>
2026-05-10 14:51:55 +0400

    Add AD on footer (vk)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,15 @@
 export default function Footer() {
   return (
     <footer>
+      <script async src="https://ad.mail.ru/static/ads-async.js"></script>
+      <ins
+        class="mrg-tag"
+        className="display:inline-block;width:auto;height:300px"
+        data-ad-client="ad-2008502"
+        data-ad-slot="2008502"
+      ></ins>
+      <script>(MRGtag = window.MRGtag || []).push({});</script>
+
       <div className="bg-gray-900 text-white w-full transition-all duration-300 text-center text-sm pt-5">
         <p className="font-bold">
           © {new Date().getFullYear()} use-tools.ru — простые веб-инструменты
